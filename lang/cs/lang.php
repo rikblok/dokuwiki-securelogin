392bab226501e7bcff80c9a65579fd9cce63aae6
Mikhail I. Izmestev <izmmishao5@gmail.com>
2009-09-01 19:24:48 +0000

    Jan Hána * add Czech language

---
+++ lang/cs/lang.php
@@ -0,0 +1,16 @@
+<?php
+$lang['generate_key'] = 'Generovat nový pár klíčů';
+$lang['generate'] = 'Generovat';
+$lang['set'] = 'Nastavit';
+$lang['key_length'] = 'Síla klíče';
+$lang['public_key'] = 'Veřejný klíč';
+$lang['key_modulus'] = 'Modulo klíče';
+$lang['key_exponent'] = 'Exponent veřejného klíče';
+$lang['test_key'] = 'Testovací klíč';
+$lang['test_message'] = 'Testovací zpráva';
+$lang['test'] = 'Test';
+$lang['use_securelogin'] = 'Bezpečné přihláčení';
+$lang['use_secureupdate'] = 'Použij bezpečný přenos hesla';
+$lang['securelogin_conf'] = 'Nastavení bezpečného přihlašování';
+$lang['sample_message'] = 'Toto je testovací zpráva';
+?>
